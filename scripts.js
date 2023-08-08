b390bf14a921313efaabf09ba7e22063ec994514
Silvio Souza <sjblu7@gmail.com>
2023-08-08 07:23:59 -0400

    First commit - Inicial files

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const generatePassword = () => {
+  const chars =
+    "abcdefghjklmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789?!@&*()[]";
+
+  let password = "";
+
+  for (let i = 0; i < 8; i++) {
+    const randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber + 1);
+  }
+
+  //   console.log(chars.length);
+  console.log(password);
+};
+
+generatePassword();
