9b8fd68d88194d88c96ee87a42811b56d6bdcdb0
Silvio Souza <sjblu7@gmail.com>
2023-08-08 07:45:13 -0400

    backend completed

--- scripts.js
+++ scripts.js
@@ -1,16 +1,33 @@
+const inputEl = document.querySelector("#password");
+let passwordLength = 16;
+
 const generatePassword = () => {
   const chars =
     "abcdefghjklmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789?!@&*()[]";
 
   let password = "";
 
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < passwordLength; i++) {
     const randomNumber = Math.floor(Math.random() * chars.length);
     password += chars.substring(randomNumber, randomNumber + 1);
   }
 
-  //   console.log(chars.length);
+  inputEl.value = password;
+
   console.log(password);
 };
+
+const copy = () => {
+  navigator.clipboard.writeText(inputEl.value);
+};
+
+const passwordLengthEl = document.querySelector("#password-length");
+passwordLengthEl.addEventListener("input", () => {
+  passwordLength = passwordLengthEl.value;
+  generatePassword();
+});
+
+const buttonCopyEL = document.querySelector("#copy");
+buttonCopyEL.addEventListener("click", copy);
 
 generatePassword();
