4682a73bd59b0f68b41ce85639b32e87e01a2bee
Silvio Souza <sjblu7@gmail.com>
2023-08-11 08:47:25 -0400

    added buttons and slider logics and finished page style

--- scripts.js
+++ scripts.js
@@ -1,9 +1,28 @@
 const inputEl = document.querySelector("#password");
+const upperCaseCheckEl = document.querySelector("#uppercase-check");
+const numberCheckEl = document.querySelector("#number-check");
+const symbolCheckEl = document.querySelector("#symbol-check");
+
 let passwordLength = 16;
 
 const generatePassword = () => {
-  const chars =
-    "abcdefghjklmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789?!@&*()[]";
+  let chars = "abcdefghjklmnpqrstuvwxyz";
+
+  const upperCaseChars = "ABCDEFGHJKLMNPQRSTUVWXYZ";
+  const numberChars = "123456789";
+  const symbolChars = "?!@&*()[]";
+
+  if (upperCaseCheckEl.checked) {
+    chars += upperCaseChars;
+  }
+
+  if (numberCheckEl.checked) {
+    chars += numberChars;
+  }
+
+  if (symbolCheckEl.checked) {
+    chars += symbolChars;
+  }
 
   let password = "";
 
@@ -24,8 +43,15 @@ const copy = () => {
 const passwordLengthEl = document.querySelector("#password-length");
 passwordLengthEl.addEventListener("input", () => {
   passwordLength = passwordLengthEl.value;
+  document.querySelector("#password-length-text").innerText = passwordLength;
   generatePassword();
 });
+
+upperCaseCheckEl.addEventListener("click", generatePassword);
+numberCheckEl.addEventListener("click", generatePassword);
+symbolCheckEl.addEventListener("click", generatePassword);
+
+document.querySelector("#renew").addEventListener("click", generatePassword);
 
 document.querySelector("#copy-1").addEventListener("click", copy);
 document.querySelector("#copy-2").addEventListener("click", copy);
